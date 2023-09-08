2dd6ecd10da3ac7557bd2dfff68631079a21600d
onopotoh <144422945+onopotoh@users.noreply.github.com>
2023-09-09 01:01:29 +0700

    Update translate.js

--- translate.js
+++ translate.js
@@ -1,58 +1,63 @@
+function createBingTranslateWidget() {
 function createGoogleTranslateWidget() {
-    // Create the translateDiv and add it to the DOM
-    const translateDiv = createTranslateDiv();
-    const targetDiv = document.querySelector('.one-page-cover-info');
-    insertAfter(translateDiv, targetDiv);
-
-    // Create and append the translation script
-    createTranslationScript();
-}
-
-function createTranslateDiv() {
     const translateDiv = document.createElement('div');
+    translateDiv.id = 'bing_translate_element';
     translateDiv.id = 'google_translate_element';
-    return translateDiv;
-}
 
-function insertAfter(newNode, referenceNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-}
+    const targetDiv = document.querySelector('.one-page-cover-info');
 
-function createTranslationScript() {
+    targetDiv.parentNode.insertBefore(translateDiv, targetDiv.nextSibling);
+
+
+
+
     const script1 = document.createElement('script');
     script1.type = 'text/javascript';
+    script1.src = 'https://www.microsofttranslator.com/ajax/v3/WidgetV3.ashx?siteData=ueOIGRSKkd965FeEGM5JtQ**&ctf=False&ui=true&settings=manual&from=auto&to=en,id,ms';
     script1.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
     document.body.appendChild(script1);
 
-    if (!window.googleTranslateElementInit) {
-        window.googleTranslateElementInit = initializeGoogleTranslate;
-    }
-}
 
-function initializeGoogleTranslate() {
-    new google.translate.TranslateElement({
-        pageLanguage: 'id',
-        includedLanguages: 'en,id,ms'
-    }, 'google_translate_element');
+    if (!window.googleTranslateElementInit) {
+        window.googleTranslateElementInit = function() {
+            new google.translate.TranslateElement({
+                pageLanguage: 'id',
+                includedLanguages: 'en,id,ms'
+                //layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL // Set layout to HORIZONTAL
+            }, 'google_translate_element');
+        }
+    }
 }
 
 function checkForTargetElement() {
     const targetElement = document.querySelector('.one-page-cover-info');
+    const buttonExists = document.querySelector('#bing_translate_element');
+
     const buttonExists = document.querySelector('#google_translate_element');
 
     if (targetElement && !buttonExists) {
+        createBingTranslateWidget();
         createGoogleTranslateWidget();
     }
 }
 
-const observer = new MutationObserver(checkForTargetElement);
+@@ -27,15 +42,7 @@ const observer = new MutationObserver(() => {
+
 observer.observe(document.body, { childList: true, subtree: true });
 
 checkForTargetElement();
 
-// Add your styles
 const style = document.createElement('style');
 style.textContent = `
-    /* Your CSS styles here */
+    /* Add your custom styles for the Bing Translate widget here */
+    /* Example: iframe styles, button styles, etc. */
+    #bing_translate_element iframe {
+        display: none !important;
+    }
+    /* Add more styles as needed */
 `;
 document.head.appendChild(style);
+checkForTargetElement(); 
+  const style = document.createElement('style');
+    style.textContent = 'iframe[id=":1.container"] { display: none !important; } body {top:0!important }; .goog-logo-link{display:none !important;}.goog-te-gadget{ color:transparent!important;} .VIpgJd-ZVi9od-l4eHX-hSRGPd{display: none} .goog-te-combo { background-color:#fff; color:#000000; border: 1px solid transparent; box-shadow: 0 4px 4px rgba(0,0,0,.1); border-radius:3px; padding:6px 8px; transition: transform .3s;} .skiptranslate.goog-te-gadget{ padding-left: 60px; padding-bottom: 20px;} #goog-gt-tt #goog-gt-vt{ display: none !important; } .VIpgJd-ZVi9od-aZ2wEe-wOHMyf.VIpgJd-ZVi9od-aZ2wEe-wOHMyf-ti6hGc { display: none !important; }';
+    document.head.appendChild(style);
